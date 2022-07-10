181ca6f456330ffc205aee977273ad14ba404323
wucheyi <577301457@qq.com>
2022-07-10 18:35:18 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/login.js
@@ -0,0 +1,84 @@
+$(function() {
+  //点击“去注册账号” 的链接 
+  $('.link_reg').on('click',function(){
+    $('.login-box').hide()
+    $('.reg-box').show()
+  })
+
+  //点击“去登录账号” 的链接
+  $('.link_login').on('click',function(){
+    $('.login-box').show()
+    $('.reg-box').hide()
+  })
+
+  //从Layui中获取 from对象
+  var form = layui.form
+  var layer=layui.layer
+  /* // 通过 form.verify()函数自定义校验规则 */
+  form.verify({
+  //自定义了一个叫做 pwd 校验规则
+  pwd:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'
+  ],
+  //检验两次密码是否一致的原则
+  repwd:function(value){
+    //通过形参拿到了确认密码里的值
+    //还需要拿到密码框中的内容
+    //然后进行一次等于的判断
+    //如果判断失败，则return一个提示信息
+var pwd=$('.reg-box [name=password]').val()
+  if(pwd !== value){
+    return'两次密码不一致！'
+    }
+  } 
+})
+
+//监听注册表单的提交事件
+$('#form_reg').on('submit',function(e){
+//1.阻止默认的提交行为
+e.preventDefault()
+//2.发起Ajax的POST请求
+var data={username:$('#form_reg [name=username]').val(),password:$('#form_reg [name=password]').val()}
+
+$.post('http://www.liulongbin.top:3007/api/reguser',data,function(res){
+  if(res.status !==0){
+   /*  console.log(res.message);
+  }
+  console.log('注册成功,请登录！'); */
+   return layer.msg(res.message)
+  }
+
+  layer.msg('注册成功,请登录！')
+
+  $('.link_login').click()
+})
+
+})
+
+//监听登录表单的提交事件
+$('#form_login').submit(function(e){
+  //阻止默认提交行为
+  e.preventDefault()
+    $.ajax({
+      url:'http://www.liulongbin.top:3007/api/login',
+      method:'POST',
+      //快速获取表单中的数据
+      data:$(this).serialize(),
+      success:function(res){
+        if (res.status !== 0) {
+          return layer.msg('登录失败')  
+        }
+        layer.msg('登录成功')
+        // console.log(res.token)
+        //将登录成功得到的token值，保存到localStorage 中
+        localStorage.setItem('token',res.token)
+        //跳转到后台主页
+        location.href = '/index.html' 
+      }
+    
+
+    })
+  })
+})
+
+
+
